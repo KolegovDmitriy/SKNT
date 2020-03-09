fd0b94ae57f120137fa50a3353ed1a0dd80782a6
Dmitiy <kd_kg@mail.ru>
2020-03-10 00:34:28 +0300

    Свёрстан шаблон тарифа

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Тарифы</title>
+	<link rel="stylesheet" href="css/Style.css">
+	<link rel="shortcut icon" href="https://www.sknt.ru/i/favicon.ico">
+	<script src="https://use.fontawesome.com/0ad2a36b68.js"></script>
+	
+</head>
+<body>
+
+
+<div class="container">
+
+
+
+    <div class="tarif" >
+
+    <div class="tarif-header">
+       
+        <h2>Тариф "Земля"</h2>
+    </div>
+
+    <div class="tarif-body">
+        <div class="lable">
+            <p class="speed">50 Мбит/с</p>
+            <p class="cost">350 &ndash; 480 <i class="fa fa-rub " aria-hidden="true"></i>/мес</p>
+            <div class="tarif-options">
+            	<p>ТВ пакет "Социальный ТВ"</p>
+            	<p>Антивирус Agnitum Outpost</p>
+            </div>
+        </div>                           
+        <p class="select-tarif" > 
+            <i class="fa fa-chevron-right" aria-hidden="true"></i>
+        </p>
+    </div>  
+	 <p class="sknt"><a href="">узнать подробнее на сайте www.sknt.ru</a></p>
+    </div> 
+</div>
+	<script src="js/script.js"></script>
+	<script type="js/jquery-3.4.1.min.js"></script>	
+</body>
+</html>
